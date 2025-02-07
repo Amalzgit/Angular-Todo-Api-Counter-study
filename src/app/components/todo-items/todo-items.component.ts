0b3187bb42baaecef4dcf15c3f1811545aa68508
Amalzgit <1mailtoamal@gmail.com>
2025-02-08 01:35:54 +0530

    I just started

---
+++ src/app/components/todo-items/todo-items.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, output } from '@angular/core';
+import { Todo } from '../../model/todo.type';
+import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-items',
+  imports: [HighlightCompletedTodoDirective,UpperCasePipe],
+  templateUrl: './todo-items.component.html',
+  styleUrl: './todo-items.component.scss'
+})
+export class TodoItemsComponent {
+todo = input.required <Todo>()
+todoToggled =output<Todo>();
+
+todoClicked(){
+  this.todoToggled.emit(this.todo())
+}
+}
